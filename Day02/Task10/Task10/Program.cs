dabe2dc0479ac453d7904e9268bcb7e8650712bb
Почебыт Кирилл <imgamick@mail.ru>
2025-04-15 12:38:56 +0300

    Day2

---
+++ Day02/Task10/Task10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Двузначные числа, сумма квадратов цифр которых кратна 13:");
+
+        for (int number = 10; number <= 99; number++)
+        {
+            int tens = number / 10; 
+            int units = number % 10;
+
+            int sumOfSquares = (tens * tens) + (units * units); 
+
+            if (sumOfSquares % 13 == 0)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
---
+++ Day02/Task9/Task9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double A = Math.PI / 3; 
+        double B = 2 * Math.PI / 3;  
+        int M = 20; 
+
+        double H = (B - A) / M;
+
+        Console.WriteLine("Таблица значений функции cos(x):");
+        Console.WriteLine("X\tF(x)");
+
+        for (int i = 0; i <= M; i++)
+        {
+            double x = A + i * H; 
+            double Fx = Math.Cos(x); 
+            Console.WriteLine($"{x:F3}\t{Fx:F3}");
+        }
+    }
+}
