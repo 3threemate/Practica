bcea20daa9db5c0c5a7f9095463b7f010a1aae4d
Почебыт Кирилл <imgamick@mail.ru>
2025-04-24 08:39:01 +0300

    Day09

---
+++ Day09/Task1/Task1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+
+namespace StudentInfo
+{
+    class Program
+    {
+        struct STUDENT
+        {
+            public string surnameInitials;
+            public int groupNumber;
+            public int[] grades;
+        }
+
+        static void Main(string[] args)
+        {
+            STUDENT[] students = new STUDENT[10];
+
+            for (int i = 0; i < students.Length; i++)
+            {я
+                Console.WriteLine($"STUDENT {i + 1}:");
+                Console.Write("Введите фамилию и инициалы: ");
+                students[i].surnameInitials = Console.ReadLine();
+
+                Console.Write("Введите номер группы: ");
+                students[i].groupNumber = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Введите пять оценок через пробел:");
+                students[i].grades = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            }
+
+            SortStudentsByGroup(students);
+
+            Console.WriteLine("\nСтуденты с средним баллом > 4.0:");
+            bool hasHighAchievers = false;
+
+            foreach (var student in students)
+            {
+                double average = student.grades.Average();
+                if (average > 4.0)
+                {
+                    Console.WriteLine($"Фамилия: {student.surnameInitials}, Номер группы: {student.groupNumber}");
+                    hasHighAchievers = true;
+                }
+            }
+
+            if (!hasHighAchievers)
+            {
+                Console.WriteLine("Нет студентов со средним баллом больше 4.0.");
+            }
+
+            Console.ReadKey();
+        }
+
+        static void SortStudentsByGroup(STUDENT[] students)
+        {
+            STUDENT temp;
+
+            for (int i = 0; i < students.Length; i++)
+            {
+                for (int j = i + 1; j < students.Length; j++)
+                {
+                    if (students[i].groupNumber > students[j].groupNumber)
+                    {
+                        temp = students[i];
+                        students[i] = students[j];
+                        students[j] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Day09/Task2/Task2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+enum Post
+{
+    Manager = 160,
+    Engineer = 150,
+    Technician = 140,
+    Clerk = 130,
+    Director = 170
+}
+
+class Accauntant
+{
+    public bool AskForBonus(Post worker, int hoursWorked)
+    {
+        int requiredHours = (int)worker;
+        if (hoursWorked > requiredHours)
+        {
+            return true;
+        }
+        return false;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Accauntant accauntant = new Accauntant();
+
+        Console.WriteLine("Введите профессию (Manager, Engineer, Technician, Clerk, Director):");
+        Post worker = (Post)Enum.Parse(typeof(Post), Console.ReadLine()!);
+
+        Console.WriteLine("Введите количество отработанных часов:");
+        int hoursWorked = int.Parse(Console.ReadLine()!);
+
+        bool bonus = accauntant.AskForBonus(worker, hoursWorked);
+        Console.WriteLine($"Положена ли премия {worker}: {bonus}");
+    }
+}
