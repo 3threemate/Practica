875e9945d7b7ff640d2bb231e8dba5ad229087e6
Почебыт Кирилл <imgamick@mail.ru>
2025-05-02 21:34:49 +0300

    Day14

---
+++ Day14/Task3/Task3/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static int A, N;
+    static object locker = new object();
+
+    static void Main()
+    {
+        Console.Write("Введите значение A: ");
+        A = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите значение N: ");
+        N = int.Parse(Console.ReadLine());
+
+        Thread thread1 = new Thread(SumNumbers);
+        Thread thread2 = new Thread(SumNumbers);
+        Thread thread3 = new Thread(MultiplyNumbers);
+
+        thread1.Start();
+        thread2.Start();
+        thread3.Start();
+
+        thread1.Join();
+        thread2.Join();
+        thread3.Join();
+
+        Console.WriteLine("Все потоки завершены.");
+    }
+
+    static void SumNumbers()
+    {
+        int sum = 0;
+        for (int i = 1; i <= N; i++)
+        {
+            lock (locker) 
+            {
+                sum += A + i;
+                Console.WriteLine($"Поток {Thread.CurrentThread.ManagedThreadId}: Сумма = {sum}");
+                Thread.Sleep(100); 
+            }
+        }
+    }
+
+    static void MultiplyNumbers()
+    {
+        int product = 1;
+        for (int i = 1; i <= N; i++)
+        {
+            product *= A * i;
+            Console.WriteLine($"Поток {Thread.CurrentThread.ManagedThreadId}: Произведение = {product}");
+            Thread.Sleep(200); 
+        }
+    }
+}
---
+++ Day14/Task4/Task4/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+
+class Program
+{
+    static int[] numbers;
+    static int totalSum = 0;
+    static object locker = new object();
+
+    static void Main()
+    {
+        Console.Write("Введите количество элементов массива: ");
+        int size = int.Parse(Console.ReadLine());
+        numbers = new int[size];
+
+        Random random = new Random();
+        for (int i = 0; i < size; i++)
+        {
+            numbers[i] = random.Next(1, 100); 
+        }
+
+        Console.Write("Введите количество потоков: ");
+        int threadCount = int.Parse(Console.ReadLine());
+
+        Thread[] threads = new Thread[threadCount];
+        int chunkSize = size / threadCount;
+
+        for (int i = 0; i < threadCount; i++)
+        {
+            int start = i * chunkSize;
+            int end = (i == threadCount - 1) ? size : (start + chunkSize);
+            threads[i] = new Thread(() => ComputePartialSum(start, end));
+            threads[i].Start();
+        }
+
+        foreach (Thread thread in threads)
+        {
+            thread.Join();
+        }
+
+        Console.WriteLine($"Общая сумма четных чисел: {totalSum}");
+    }
+
+    static void ComputePartialSum(int start, int end)
+    {
+        int partialSum = 0;
+        for (int i = start; i < end; i++)
+        {
+            if (numbers[i] % 2 == 0)
+            {
+                partialSum += numbers[i];
+            }
+        }
+
+        lock (locker)
+        {
+            totalSum += partialSum;
+        }
+    }
+}
