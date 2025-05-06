e7a8e5f84c7f8b7fddaf2fc4a7914a35920357a3
Почебыт Кирилл <imgamick@mail.ru>
2025-05-06 20:05:31 +0300

    Day17

---
+++ Day17/LibraryTask2/LibraryTask2/Class1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LibraryTask2
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}: {Price} руб.";
+        }
+    }
+
+    public class Cart
+    {
+        private List<Product> products = new List<Product>();
+
+        public void AddProduct(Product product)
+        {
+            products.Add(product);
+            Console.WriteLine($"{product.Name} добавлен в корзину.");
+        }
+
+        public void RemoveProduct(Product product)
+        {
+            if (products.Remove(product))
+                Console.WriteLine($"{product.Name} удален из корзины.");
+            else
+                Console.WriteLine($"{product.Name} не найден в корзине.");
+        }
+
+        public double GetTotalPrice()
+        {
+            double total = 0;
+            foreach (var product in products)
+            {
+                total += product.Price;
+            }
+            return total;
+        }
+
+        public void ShowCart()
+        {
+            Console.WriteLine("Содержимое корзины:");
+            foreach (var product in products)
+            {
+                Console.WriteLine(product);
+            }
+            Console.WriteLine($"Итоговая стоимость: {GetTotalPrice()} руб.");
+        }
+    }
+}
---
+++ Day17/TestTask2/TestTask2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using StoreLibrary;
+
+class Program
+{
+    static void Main()
+    {
+        Product product1 = new Product("Молоко", 2.5);
+        Product product2 = new Product("Хлеб", 1.2);
+        Product product3 = new Product("Яблоки", 3.8);
+
+        Cart cart = new Cart();
+        cart.AddProduct(product1);
+        cart.AddProduct(product2);
+        cart.AddProduct(product3);
+
+        cart.ShowCart();
+
+        cart.RemoveProduct(product2);
+        cart.ShowCart();
+    }
+}
